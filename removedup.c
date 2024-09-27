249853e2c5331b5351a2dd365765b7b4ac986a48
thejas165 <thejasp.cs23@bmsce.ac.in>
2024-09-27 15:40:34 +0530

    Add files via upload

---
+++ removedup.c
@@ -0,0 +1,13 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int i,j=0;
+    
+    for(i=0;i<numsSize;i++)
+    {
+        if(nums[i]!=nums[j]){
+            j++;
+            nums[j]=nums[i];
+        }
+    }
+        return j+1;
+    
+}
